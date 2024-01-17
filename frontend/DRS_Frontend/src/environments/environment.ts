87e8c2c15bc2a5121163bb716a8aa6e0530d9d4d
jozla <zlatko.cikic12@gmail.com>
2024-01-17 22:13:31 +0100

    environment changed

--- frontend/DRS_Frontend/src/environments/environment.ts
+++ frontend/DRS_Frontend/src/environments/environment.ts
@@ -1,8 +1,4 @@
-const DOMAIN = "http://spasic.co.rs"
-const PORT = ":8080"
-const TAIL = "/api"
-
 export const environment = {
   production: false,
-  backendBaseUrl: process.env["BASE_BACKEND_URL"]
+  backendBaseUrl: "http://postgressql-flask-app-1:5000/api"
 };
